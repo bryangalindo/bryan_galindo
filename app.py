e7b092f9a265c1e37282747c3e766de70cf164b6
bryangalindo <galindo.bryan08@gmail.com>
2022-07-20 13:25:53 -0400

    add breakout levels project ep

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ def home():
 @app.route('/resume')
 def resume():
     return redirect('https://drive.google.com/file/d/1QRPfRh5tIq86xZE-ODQR6f6GQnR5UAlN/view?usp=sharing')
+
+
+@app.route('/breakouts')
+def breakouts():
+    return redirect('https://archive.ph/YNd5H')
