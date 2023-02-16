fbda1d0f7e626721bd03d9544375d1aa85dd7515
bryangalindo <galindo.bryan08@gmail.com>
2023-02-16 14:28:09 -0600

    test pre-commit

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import render_template, Flask, redirect
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/resume')
+@app.route("/resume")
 def resume():
-    return redirect('https://drive.google.com/file/d/1kTTryC5yijLwlhN7mldimq2y46l__hFn/view?usp=sharing')
+    return redirect(
+        "https://drive.google.com/file/d/1kTTryC5yijLwlhN7mldimq2y46l__hFn/view?usp=sharing"
+    )
 
 
-@app.route('/breakoutlevels')
+@app.route("/breakoutlevels")
 def breakouts():
-    return redirect('https://archive.ph/YNd5H')
+    return redirect("https://archive.ph/YNd5H")
 
 
-@app.route('/ebayscraper')
+@app.route("/ebayscraper")
 def ebayscraper():
-    return redirect('https://github.com/bryangalindo/scrape/blob/master/ebayscrape.py')
+    return redirect("https://github.com/bryangalindo/scrape/blob/master/ebayscrape.py")
 
 
-@app.route('/tamalemkt')
+@app.route("/tamalemkt")
 def tamalemkt():
-    return redirect('https://tamalemkt.com')
+    return redirect("https://tamalemkt.com")
